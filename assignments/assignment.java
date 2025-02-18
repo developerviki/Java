6594f74af3725af1e4349fe2245865d0930f41b3
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-02-18 16:38:19 +0530

    summit

---
+++ assignments/assignment.java
@@ -0,0 +1,141 @@
+import java.util.*;
+
+public class assignment {
+    public static void main(String[] args) {
+
+        // Q1. Write a Java program to get a number from the user and print whether it
+        // is positive or negative.
+
+        // Scanner checkNumber = new Scanner(System.in);
+        // System.out.println("Enter a number");
+        // int number = checkNumber.nextInt();
+        // if (number>0) {
+        // System.out.println("Positive Number");
+        // } else {
+        // System.out.println("Negetive Number");
+        // }
+
+        // Q2. Finish the following code so that it prints "You have a fever" if the
+        // temperature is above 100 and otherwise prints "You don't have a fever".
+
+        // public class Solution {
+        // public static void main(String[] args) {
+        // double temp = 103.5;
+        // // Complete the code here
+        // }
+        // }
+        // double temp = 103.5;
+        // if (temp > 100) {
+        // System.out.println("You have a fever");
+        // } else {
+        // System.out.println("You don't have a fever");
+        // }
+
+        // Q3. Write a Java program to input a week number (1-7) and print the
+        // corresponding day of the week using a switch case.
+
+        // Scanner inputValue = new Scanner(System.in);
+        // System.out.println("Enter a number");
+        // int day = inputValue.nextInt();
+
+        // switch (day) {
+        // case 1:
+        // System.out.println("Monday");
+        // break;
+        // case 2:
+        // System.out.println("Tuesday");
+        // break;
+        // case 3:
+        // System.out.println("Wednesday");
+        // break;
+        // case 4:
+        // System.out.println("Thursday");
+        // break;
+        // case 5:
+        // System.out.println("Friday");
+        // break;
+        // case 6:
+        // System.out.println("Saturday");
+        // break;
+        // case 7:
+        // System.out.println("Sunday");
+        // break;
+
+        // default:
+        // System.out.println("Invalid input ! Please try again.");
+        // break;
+        // }
+
+        // Q4. What will be the value of x and y in the following program?
+
+        // public class Solution {
+        // public static void main(String args[]) {
+        // int a = 63, b = 36;
+        // boolean x = (a < b) ? true : false;
+        // int y = (a > b) ? a : b;
+        // }
+        // }
+
+        // int a = 63, b = 36;
+        // boolean x = (a < b) ? true : false;
+        // int y = (a > b) ? a : b;
+        // System.out.println(x);
+        // System.out.println(y);
+
+        // Q7. Write a program that reads a set of integers and then prints the sum of
+        // the even and odd integers separately.
+
+        // Scanner inputValueOddEven = new Scanner(System.in);
+        // System.out.println("Enter Your Number For Check Odd And Even");
+        // int value = inputValueOddEven.nextInt();
+        // if (value%2 == 0) {
+        // System.out.println("This number is even " + value);
+        // } else {
+        // System.out.println("This number is odd " + value);
+        // }
+
+        // Q8. Write a program to find the factorial of any number entered by the user.
+
+        // Hint: The factorial of a number n is calculated as:
+
+        // n!=n×(n−1)×(n−2)×...×1
+        // Factorial exists only for positive numbers, and we define:
+
+        // 0!=1
+        // 1!=1
+        // 2!=2
+        // 3!=6
+        // 4!=24, and so on.
+        // Note: Do not confuse factorial (!) with the "not equal to" operator (!=), as
+        // they are not the same.
+
+        // Scanner inputFactorialValue = new Scanner(System.in);
+        // System.out.println("Enter Your Factorial number ");
+        // int sum = 1;
+        // int value = inputFactorialValue.nextInt();
+        // for (int i = 1; i <= value; i++) {
+        // sum*=i;
+        // }
+        // System.out.println(sum);
+
+        // Q9. Write a program to print the multiplication table of a number N, entered
+        // by the user.
+
+        // Scanner multiplicationNumber = new Scanner(System.in);
+        // System.out.println("Enter Your number and get table :");
+        // int tableValue = multiplicationNumber.nextInt();
+        // for (int i = 1; i <=10; i++) {
+
+        // System.out.printf("%d x %d = %d%n" , tableValue, i, tableValue * i);
+        // }
+
+        // Scanner sc = new Scanner(System.in);
+        // int age = sc.nextInt();
+        // if(age%4==0 && age%100!=0 || age%400==0){
+        // System.out.println("leep year");
+        // }else{
+        // System.out.println("no leep year");
+        //  }
+
+    };
+}
---
+++ assignments/assignment2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class assignment2 {
+    public static void main(String[] args) {
+        
+     /*
+     ### *Question 1:*  
+     Write a Java method to compute the *average of three numbers*.
+     */
+
+     
+
+
+    }
+}
