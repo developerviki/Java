e7221c44fee1fede7ae25c924b6dd030d473a284
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-02-19 10:22:36 +0530

    next question

--- start assignment/assignment.java
+++ start assignment/assignment.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 
 public class assignment {
     public static void main(String[] args) {
--- start assignment/assignment2.java
+++ start assignment/assignment2.java
@@ -1,4 +1,6 @@
-import java.util.*;
+// import java.util.*;
+
+import java.util.Scanner;
 
 public class assignment2 {
     public static void main(String[] args) {
@@ -8,8 +10,11 @@ public class assignment2 {
      Write a Java method to compute the *average of three numbers*.
      */
 
-     
+     Scanner input1 = new Scanner(System.in);
+     Scanner input2 = new Scanner(System.in);
+     Scanner input3 = new Scanner(System.in);
 
+     
 
     }
 }
